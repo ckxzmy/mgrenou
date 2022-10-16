0780c32c8e870458bc2f0094775473786a3dbc2e
ckxzmy <ckx_0909@163.com>
2022-10-16 18:24:42 +0800

    secretKey

--- src/main/java/com/example/mgdoll/util/QiNiuUtil.java
+++ src/main/java/com/example/mgdoll/util/QiNiuUtil.java
@@ -1,12 +1,18 @@
 package com.example.mgdoll.util;
 
+import com.google.gson.Gson;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
 import com.qiniu.storage.Configuration;
 import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
 import com.qiniu.util.Auth;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileInputStream;
 
 @Component
 public class QiNiuUtil {
@@ -14,8 +20,8 @@ public class QiNiuUtil {
 
     @Value("${qiniu.accessKey}")
     private String accessKey;
-    @Value("${qiniu.accessSecretKey}")
-    private String accessSecretKey;
+    @Value("${qiniu.secretKey}")
+    private String secretKey;
     @Value("${qiniu.bucket}")
     private String bucket;
 
@@ -26,11 +32,30 @@ public class QiNiuUtil {
         //...其他参数参考类注释
         UploadManager uploadManager = new UploadManager(cfg);
         try {
-            Auth auth = Auth.create(accessKey, accessSecretKey);
+            Auth auth = Auth.create(accessKey, secretKey);
             upToken = auth.uploadToken(bucket);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         return upToken;
     }
+
+//    public String upload(FileInputStream file){
+//        Configuration cfg = new Configuration(Region.huanan());
+//        //...其他参数参考类注释
+//        UploadManager uploadManager = new UploadManager(cfg);
+//            Auth auth = Auth.create("ZaVxlO5lMeEKbHe0EuajPpPp7JGTE1RjXALQuBfD", "Q5OsVgk5rIxsOjo3Bmbsolcng6eTfupQ_U_E5a0p");
+//            String upToken = auth.uploadToken("mgdoll-test");
+////            path = Constant.domainPicture;
+//        try {
+//            Response response = uploadManager.put(file, null, upToken, null, null);
+//
+//            // 解析上传成功的结果
+//            DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+//        } catch (QiniuException e) {
+//            e.printStackTrace();
+//        }
+////        return path + putRet.key;
+//        return null;
+//    }
 }
